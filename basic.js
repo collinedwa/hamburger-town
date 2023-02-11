953dbbc754b8dea42e3ae4fc9eca10de8b7c0eae
Collin <collinedwards@proton.me>
2023-02-10 17:44:23 -0800

    removed JS; replaced with css targeting; added animations;

--- basic.js
+++
@@ -1,9 +0,0 @@
-function displayPanel(id) {
-    resetList = document.getElementsByTagName('panel');
-
-    for (item of resetList){
-        item.style.display = "none"
-    };
-
-    document.getElementById(id).style.display = "block";
-}
